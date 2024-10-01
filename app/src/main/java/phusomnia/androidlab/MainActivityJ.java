fc69dde25378f457f4670e2141d1ffda37aca486
phusomnia <tuanhphu2004thcs@gmail.com>
2024-10-01 21:19:20 +0700

    Bái

--- app/src/main/java/phusomnia/androidlab/MainActivityJ.java
+++ app/src/main/java/phusomnia/androidlab/MainActivityJ.java
@@ -1,4 +1,20 @@
 package phusomnia.androidlab;
 
-public class MainActivityJ {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivityJ extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ImageView myImageView = new ImageView(this);
+        myImageView.setImageResource(R.drawable.logo__padoru);
+        setContentView(myImageView);
+    }
 }
